c9197483d0c4bf8e977f6db0f6df4a4cefb89b93
Tbouder <tbouder@student.42.fr>
2016-10-05 11:08:13 +0200

    Looking for minimal code

--- site/src/components/IndexPage.js
+++ site/src/components/IndexPage.js
@@ -6,24 +6,20 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/03 22:53:03 by tbouder           #+#    #+#             */
-/*   Updated: 2016/10/04 20:50:04 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/10/04 23:52:41 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import React	from 'react';
 
-class IndexPage extends React.Component
+export default class IndexPage extends React.Component
 {
 	render()
 	{
 		return (
 			<div>
-				<div>
-					hello
-				</div>
+				<div>Hello World!</div>
 			</div>
 		);
 	}
 }
-
-export default IndexPage;
