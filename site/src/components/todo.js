388a4263fe43e63a3a24bb17722b406d67da81a8
Tbouder <tbouder@protonmail.com>
2016-10-07 22:47:29 +0200

    WIP with database

--- site/src/components/todo.js
+++ site/src/components/todo.js
@@ -6,12 +6,13 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/03 22:53:03 by tbouder           #+#    #+#             */
-/*   Updated: 2016/10/06 19:10:11 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/10/07 22:47:15 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import React	from 'react';
 import {Link}	from 'react-router';
+import Firebase from 'firebase';
 
 export default class todo extends React.Component
 {
@@ -22,6 +23,20 @@ export default class todo extends React.Component
 		this.ft_add_task = this.ft_add_task.bind(this);
 		this.ft_del_task = this.ft_del_task.bind(this);
 		this.ft_change_text = this.ft_change_text.bind(this);
+		this.ft_load = this.ft_load.bind(this);
+
+		/*Load todo from database*/
+		this.ft_load();
+
+		var THIS = this;
+		var database = firebase.database().ref("todo_list");
+
+		database.on("value", function (snapshot)
+		{
+			var	nb_elem = snapshot.numChildren();
+			THIS.setState({todo: nb_elem, buffer: nb_elem});
+		});
+
 	}
 
 	ft_add_task()
@@ -30,30 +45,43 @@ export default class todo extends React.Component
 		let text = this.state.value;
 		if (text == "")
 			return;
+		/*******************/
+		var database = firebase.database();
+		firebase.database().ref('/todo_list/' + id).set(
+		{
+			text: {text}
+		});
+		/******************/
 		this.state.comps.push(
 			<div className="card" key={id} id={id}>
 				<div className="content">
-					<div className="header">Todo n_{id}</div>
-					<div className="description">
-						{this.state.value}
-					</div>
+					<div className="header">Todo number {id}</div>
+					<div className="description">{this.state.value}</div>
 				</div>
 					<div className="ui bottom attached button green" onClick={this.ft_del_task.bind(this, id)} id={id}>
 						<i className="checkmark icon"></i>
 						Done
 					</div>
-
 			</div>
 		);
 		this.setState({todo: this.state.todo + 1, buffer: id + 1});
 		this.setState({value: ""});
 	}
 
+	ft_add_task_enter(e)
+	{
+		if (e.charCode == 13 || e.keyCode == 13)
+			{this.ft_add_task()};
+	}
+
 	ft_del_task(id)
 	{
 		delete this.state.comps[id];
+
+		var database = firebase.database();
+		var ref = database.ref("todo_list/" + id);
+		ref.remove();
 		this.setState({todo: this.state.todo - 1});
-		this.setState({comps: this.state.comps});
 	}
 
 	ft_change_text(event)
@@ -61,11 +89,42 @@ export default class todo extends React.Component
 		this.setState({value: event.target.value});
 	}
 
-	handleKeyPress(event)
+	ft_load()
 	{
-	  if(event.key == 'Enter'){
-	    console.log('enter press here! ')
-	  }
+		var THIS = this;
+		var datas = [];
+		var database = firebase.database();
+		var ref = database.ref("todo_list").orderByKey();
+
+		ref.on("value", function (snapshot)
+		{
+			snapshot.forEach(function(childSnapshot)
+			{
+				let key = childSnapshot.key;
+				let text = childSnapshot.val().text.text;
+				datas[key] = text;
+			});
+			datas.forEach(logArrayElements, THIS);
+		});
+
+		function logArrayElements(element, index, array)
+		{
+			let id = index;
+			let text = element;
+			THIS.state.comps.push(
+				<div className="card" key={id} id={id}>
+					<div className="content">
+						<div className="header">Todo number {id}</div>
+						<div className="description">{text}</div>
+					</div>
+						<div className="ui bottom attached button green" onClick={THIS.ft_del_task.bind(THIS, id)} id={id}>
+							<i className="checkmark icon"></i>
+							Done
+						</div>
+				</div>
+			);
+			// this.setState({todo: THIS.state.todo + 1, buffer: THIS.state.buffer + 1});
+		}
 	}
 
 	render()
@@ -76,14 +135,14 @@ export default class todo extends React.Component
 					<div className="ui cards centered">
 						<div className="card">
 							<div className="content">
-								<div className="header centered">Add a todo : {this.state.todo}</div>
-							</div>
-							<div className="description">
-								<div className="ui fluid input">
-									<input type="text" value={this.state.value} placeholder="What do you want todododo ?" onChange={this.ft_change_text} />
+								<div className="header">You have {this.state.todo} todo to do</div>
+								<div className="description">
+									<div className="ui fluid input">
+										<input type="text" value={this.state.value} placeholder="What do you want todododo ?" onChange={this.ft_change_text} onKeyPress={this.ft_add_task_enter.bind(this)}/>
+									</div>
 								</div>
 							</div>
-							<div className="ui bottom attached button" onClick={this.ft_add_task} onKeyPress={this.handleKeyPress}>
+							<div className="ui bottom attached button" onClick={this.ft_add_task}>
 								<i className="add icon"></i>
 							</div>
 						</div>
