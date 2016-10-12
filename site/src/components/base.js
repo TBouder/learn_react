6444d64f1ac32ee372ff5797df54bb2521c9b60c
Tbouder <tbouder@student.42.fr>
2016-10-12 16:54:53 +0200

    Add account + login

--- site/src/components/base.js
+++ site/src/components/base.js
@@ -6,15 +6,36 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/03 22:48:14 by tbouder           #+#    #+#             */
-/*   Updated: 2016/10/10 16:09:01 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/10/12 12:28:03 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import React	from 'react';
 import {Link}	from 'react-router';
+import Firebase from 'firebase';
 
 export default class base extends React.Component
 {
+	constructor()
+	{
+		super();
+		var THIS = this;
+		this.state = {user_is_logged_in: ""};
+
+		firebase.auth().onAuthStateChanged(function(user)
+		{
+			if (user)
+			{
+				function ft_display_username(user)
+				{
+					return (<h2 className="ui center aligned icon header">Hello {user.displayName}</h2>)
+				}
+				THIS.setState({user_is_logged_in: ft_display_username(user)});
+				console.log(user.uid);
+			}
+		});
+	}
+
 	render()
 	{
 		return (
@@ -22,6 +43,7 @@ export default class base extends React.Component
 				<header>
 					<img className="ui centered image padding_two" src="/img/logo.png" />
 					<Link to="/"><h2 className="text_center">React + Node_js : server side</h2></Link>
+					{this.state.user_is_logged_in}
 				</header>
 
 				<div>{this.props.children}</div>
